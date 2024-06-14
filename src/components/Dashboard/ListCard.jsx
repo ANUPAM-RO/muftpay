a46d42bdb04efb4e68afae7f77749157e0926ac9
anupam <anupamroy575@gamail.com>
2024-06-14 17:59:59 +0530

    update status

--- src/components/Dashboard/ListCard.jsx
+++ src/components/Dashboard/ListCard.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { formatDate } from "../../utiils";
 
 const ListCard = ({ data }) => {
-
   return (
     <div className="flex justify-between">
       <div className="flex gap-3">
-        <img src={data?.userId?.image} alt="" className="rounded-full w-12 h-12" />
+        <img
+          src={data?.userId?.image}
+          alt=""
+          className="rounded-full w-12 h-12"
+        />
         <div>
           <p style={{ color: "#222222" }}>{data?.userId?.name}</p>
           <p style={{ color: "rgba(34, 34, 34, 0.5)" }}>
@@ -16,9 +19,15 @@ const ListCard = ({ data }) => {
       </div>
       <div className="text-right">
         <p className="font-semibold" style={{ color: "#222222" }}>
-        ₹{data?.amount}
+          ₹{data?.amount}
         </p>
-        <p style={{ color: "#24AD6B" }}>Completed</p>
+        <p
+          className={
+            data?.status === "Completed" ? "text-[#10B981]" : "text-[#FFB444]"
+          }
+        >
+          {data?.status}
+        </p>
       </div>
     </div>
   );
